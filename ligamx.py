0d45fefd443672541dbb8dbef5f5125d63ff1d86
Alberto Cardenas <betoalien@gmail.com>
2021-04-16 00:10:08 -0500

    update

--- ligamx.py
+++ ligamx.py
@@ -32,17 +32,6 @@ for i in pt:
         break
     count += 1
 
-#Puntos en Casa
-ptcasa = soup.find_all('td', class_='destacado2')
 
-puntoscasa = list()
-count = 0
-for i in ptcasa:
-    if count < 18:
-        puntoscasa.append(i.text)
-    else:
-        break
-    count += 1
-
-df = pd.DataFrame({'Nombre': equipos, 'Puntos': puntos,'En Casa': puntoscasa}, index=list(range(1,19)))
+df = pd.DataFrame({'Nombre': equipos, 'Puntos': puntos}, index=list(range(1,19)))
 df.to_csv('ClasificacionLigaMX.csv', index=True)
